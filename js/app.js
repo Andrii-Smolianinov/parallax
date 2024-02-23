09e5f357b40d976ed6c36add0d62539375e7f2b7
Andrii-Smolianinov <97645771+Andrii-Smolianinov@users.noreply.github.com>
2024-02-23 23:51:31 +0200

    added ScrollTrigger, ScrollSmoother

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 window.addEventListener("scroll", (e) => {
-  document.body.style.cssText = `--scrolling: ${this.scrollY}px`;
+  document.body.style.cssText += `--scrolling: ${this.scrollY}px`;
 });
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother) 
+
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content'
+})
